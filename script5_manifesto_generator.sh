10c17ebe8a2084beec0fd2395cf03a85a8d8f31a
divyanshushekhar2502 <divyanshushekhar2502@gmail.com>
2026-03-30 00:35:38 +0530

    Update and rename 05-manifesto.sh to script5_manifesto_generator.sh

--- script5_manifesto_generator.sh
+++ script5_manifesto_generator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Author: divyanshushekhar05-lab (24MEI10089)
+# Author: divyanshushekhar2502  (24MEI10089)
 
 echo "================================================================================"
 echo "                   Python AUDIT - MANIFESTO GENERATOR                 "

7ab24ca61eafbae1f691e8bc1a9c08bddac75d29
divyanshushekhar2502 <divyanshushekhar2502@gmail.com>
2026-03-31 19:39:46 +0530

    Update 03-auditor.sh

--- 03-auditor.sh
+++ 03-auditor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Author: divyanshushekhar05-lab (24MEI10089)
+# Author: divyanshushekhar2502 (24MEI10089)
 # ------------------------------------------------------------
 # Script: 03-auditor.sh
 # Purpose: Audit a set of core system directories plus two Python‑specific
